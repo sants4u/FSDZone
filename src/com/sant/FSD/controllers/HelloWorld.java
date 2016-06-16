46874a363f4e82dc43150980df52c952f3f0a59c
Santosh Chaitanya <contactme.santhu@gmail.com>
2016-06-16 15:48:16 +0530

    FSDZone - At its absolute sctrach stage. With all basic maven dependencies configured

---
+++ src/com/sant/FSD/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sant.FSD.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorld {
+
+	@RequestMapping("/helloWorld")
+    public String sayHello(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "helloWorld";
+    }
+}
